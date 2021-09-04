fe4b9173cf74f3314d8dc444d3b90ef8157b03da
Genta Kamitani <oftn.mofumofu@gmail.com>
2021-09-05 07:36:25 +0900

    Feat: add Beats and Play

--- jankensheep.go
+++ jankensheep.go
@@ -6,7 +6,7 @@ import "fmt"
 type Sign int
 
 // All hand signs.
-// Note that we internally use modulo calculation to implement the game, so they are defined explcitly instead of using `iota`.
+// Note that we internally use modulo calculation to implement the game rule, so they are defined explcitly instead of using `iota`.
 const (
 	Rock     Sign = 0
 	Paper    Sign = 1
@@ -24,6 +24,50 @@ func (s Sign) GoString() string {
 	default:
 		panic(errInvalidSign(s))
 	}
+}
+
+// Beats returns `true` if `s` beats `t`; otherwise it returns `false`.
+func (s Sign) Beats(t Sign) bool {
+	return ((t + 1) % 3) == s
+}
+
+// Play returns indices of winners of this game `in O(N)`` time complexity.
+// It panics when `len(signs) < 2`.
+func Play(signs []Sign) []int {
+	if len(signs) < 2 {
+		panic("jankensheep: at least two players are needed to play the game")
+	}
+
+	// First, we decide which sign wins the game.
+	var winningSign = Sign(0)
+	someoneWins := false
+outer:
+	for _, winningSign = range []Sign{Rock, Paper, Scissors} {
+		someoneBeated := false
+		for _, s := range signs {
+			if s.Beats(winningSign) {
+				continue outer
+			} else if winningSign.Beats(s) {
+				someoneBeated = true
+			}
+		}
+		if someoneBeated {
+			someoneWins = true
+			break
+		}
+	}
+	if !someoneWins {
+		return []int{}
+	}
+
+	// Then, we decide which players are the winners.
+	winnerIndices := make([]int, 0, len(signs))
+	for i, s := range signs {
+		if s == winningSign {
+			winnerIndices = append(winnerIndices, i)
+		}
+	}
+	return winnerIndices
 }
 
 func errInvalidSign(s Sign) error {
--- jankensheep_test.go
+++ jankensheep_test.go
@@ -3,18 +3,148 @@ package jankensheep_test
 import (
 	"testing"
 
-	"github.com/genkami/jankensheep"
+	. "github.com/genkami/jankensheep"
 )
 
 func TestSignGoString(t *testing.T) {
-	assertSign := func(s jankensheep.Sign, expected string) {
+	assertSign := func(s Sign, expected string) {
 		actual := s.GoString()
 		if actual != expected {
 			t.Errorf("Sign %d: expected %s but got %s", s, expected, actual)
 		}
 	}
 
-	assertSign(jankensheep.Rock, "Rock")
-	assertSign(jankensheep.Paper, "Paper")
-	assertSign(jankensheep.Scissors, "Scissors")
+	assertSign(Rock, "Rock")
+	assertSign(Paper, "Paper")
+	assertSign(Scissors, "Scissors")
+}
+
+func TestBeats(t *testing.T) {
+	assertBeats := func(x, y Sign) {
+		if !x.Beats(y) {
+			t.Errorf("expected %#v to beat %#v, but it doesn't", x, y)
+		}
+	}
+	assertNotBeats := func(x, y Sign) {
+		if x.Beats(y) {
+			t.Errorf("expected %#v to not beat %#v, but it does", x, y)
+		}
+	}
+
+	assertNotBeats(Rock, Rock)
+	assertNotBeats(Rock, Paper)
+	assertBeats(Rock, Scissors)
+
+	assertBeats(Paper, Rock)
+	assertNotBeats(Paper, Paper)
+	assertNotBeats(Paper, Scissors)
+
+	assertNotBeats(Scissors, Rock)
+	assertBeats(Scissors, Paper)
+	assertNotBeats(Scissors, Scissors)
+}
+
+func TestPlay_TwoPlayers(t *testing.T) {
+	assertResultIs(t, []Sign{Rock, Rock}, []int{})
+	assertResultIs(t, []Sign{Rock, Paper}, []int{1})
+	assertResultIs(t, []Sign{Rock, Scissors}, []int{0})
+
+	assertResultIs(t, []Sign{Paper, Rock}, []int{0})
+	assertResultIs(t, []Sign{Paper, Paper}, []int{})
+	assertResultIs(t, []Sign{Paper, Scissors}, []int{1})
+
+	assertResultIs(t, []Sign{Scissors, Rock}, []int{1})
+	assertResultIs(t, []Sign{Scissors, Paper}, []int{0})
+	assertResultIs(t, []Sign{Scissors, Scissors}, []int{})
+}
+
+func TestPlay_ManyPlayers_SinglePlayerWins(t *testing.T) {
+	assertResultIs(t, []Sign{Rock, Scissors, Scissors}, []int{0})
+	assertResultIs(t, []Sign{Rock, Paper, Rock}, []int{1})
+	assertResultIs(t, []Sign{Paper, Paper, Scissors}, []int{2})
+
+	assertResultIs(t, []Sign{Rock, Scissors, Scissors, Scissors}, []int{0})
+	assertResultIs(t, []Sign{Rock, Scissors, Scissors, Scissors, Scissors}, []int{0})
+	assertResultIs(t, []Sign{Rock, Scissors, Scissors, Scissors, Scissors, Scissors}, []int{0})
+}
+
+func TestPlay_ManyPlayers_MultiplePlayersWin(t *testing.T) {
+	assertResultIs(t, []Sign{Rock, Rock, Scissors}, []int{0, 1})
+	assertResultIs(t, []Sign{Rock, Paper, Paper}, []int{1, 2})
+	assertResultIs(t, []Sign{Scissors, Paper, Scissors}, []int{0, 2})
+
+	assertResultIs(t, []Sign{Scissors, Paper, Scissors, Scissors}, []int{0, 2, 3})
+	assertResultIs(t, []Sign{Scissors, Paper, Scissors, Scissors, Scissors}, []int{0, 2, 3, 4})
+
+	assertResultIs(t, []Sign{Scissors, Paper, Paper, Scissors, Scissors}, []int{0, 3, 4})
+	assertResultIs(t, []Sign{Scissors, Paper, Paper, Paper, Scissors}, []int{0, 4})
+}
+
+func TestPlay_AllSignsAreTheSame(t *testing.T) {
+	assertResultIs(t, []Sign{Rock, Rock}, []int{})
+	assertResultIs(t, []Sign{Rock, Rock, Rock}, []int{})
+	assertResultIs(t, []Sign{Rock, Rock, Rock, Rock}, []int{})
+	assertResultIs(t, []Sign{Rock, Rock, Rock, Rock, Rock}, []int{})
+
+	assertResultIs(t, []Sign{Paper, Paper}, []int{})
+	assertResultIs(t, []Sign{Paper, Paper, Paper}, []int{})
+	assertResultIs(t, []Sign{Paper, Paper, Paper, Paper}, []int{})
+	assertResultIs(t, []Sign{Paper, Paper, Paper, Paper, Paper}, []int{})
+
+	assertResultIs(t, []Sign{Scissors, Scissors}, []int{})
+	assertResultIs(t, []Sign{Scissors, Scissors, Scissors}, []int{})
+	assertResultIs(t, []Sign{Scissors, Scissors, Scissors, Scissors}, []int{})
+	assertResultIs(t, []Sign{Scissors, Scissors, Scissors, Scissors, Scissors}, []int{})
+}
+
+func TestPlay_AllKindsOfSignsArePresent(t *testing.T) {
+	assertResultIs(t, []Sign{Rock, Paper, Scissors}, []int{})
+	assertResultIs(t, []Sign{Paper, Scissors, Rock}, []int{})
+	assertResultIs(t, []Sign{Scissors, Rock, Paper}, []int{})
+
+	assertResultIs(t, []Sign{Rock, Paper, Scissors, Rock}, []int{})
+	assertResultIs(t, []Sign{Rock, Paper, Scissors, Paper}, []int{})
+	assertResultIs(t, []Sign{Rock, Paper, Scissors, Scissors}, []int{})
+}
+
+func TestPlay_InsufficientNumberOfPlayers(t *testing.T) {
+	assertPanics := func(name string, thunk func()) {
+		panicked := false
+
+		func() {
+			defer func() {
+				if err := recover(); err != nil {
+					panicked = true
+				}
+			}()
+			thunk()
+		}()
+
+		if !panicked {
+			t.Errorf("%s: expected the function to panic, but it didn't", name)
+		}
+	}
+
+	assertPanics("empty", func() { Play([]Sign{}) })
+	assertPanics("single", func() { Play([]Sign{Rock}) })
+}
+
+func assertResultIs(t *testing.T, signs []Sign, expected []int) {
+	actual := Play(signs)
+	if !equalInts(expected, actual) {
+		t.Errorf("expected Play(%#v) == %#v, but got %#v", signs, expected, actual)
+	}
+}
+
+func equalInts(xs, ys []int) bool {
+	if len(xs) != len(ys) {
+		return false
+	}
+	for i, x := range xs {
+		y := ys[i]
+		if x != y {
+			return false
+		}
+	}
+	return true
 }
